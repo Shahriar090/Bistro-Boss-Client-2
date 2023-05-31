57cef2ed4ad603a4f6949956b982f3a33cec4148
Shahriar090 <shahriarh655@gmail.com>
2023-05-31 13:22:50 +0600

    added more section and some responsivness

---
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,17 @@
+
+
+const MenuItem = ({item}) => {
+    const {name, image, price, recipe} = item;
+    return (
+        <div className="flex space-x-4 px-4 sm:px-8 ">
+            <img style={{borderRadius: '0 200px 200px 200px'}} className="w-[80px] sm:w-[100px]" src={image} alt="" />
+            <div>
+                <h3 className="uppercase">{name}-------------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-600">${price}</p>
+        </div>
+    );
+};
+
+export default MenuItem;
