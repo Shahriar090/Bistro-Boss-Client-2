7fbf1f20c96346e5cf4f421130878c675d15164b
Shahriar090 <shahriarh655@gmail.com>
2023-06-01 22:13:10 +0600

    loaded data by using custom hook

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,38 +1,27 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
-
+import useMenu from "../../../Hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
 
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data=>{
-            const popularItems = data.filter(item=>item.category==='popular')
-            setMenu(popularItems)
-        })
-    },[])
-    return (
-        <section className="mb-12">
-
-            <SectionTitle
-            heading={'From Our Menu'}
-            subHeading={'Popular Items'}
-            ></SectionTitle>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 ">
-                {
-                    menu.map(item => <MenuItem
-                    key={item._id}
-                    item={item}
-                    ></MenuItem>)
-                }
-            </div>
-            <button className="btn btn-outline border-0 border-b-2 mt-5 text-yellow-500">View Full Menu</button>
-            
-        </section>
-    );
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        heading={"From Our Menu"}
+        subHeading={"Popular Items"}
+      ></SectionTitle>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 ">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <button className="btn btn-outline border-0 border-b-2 mt-5 text-yellow-500">
+        View Full Menu
+      </button>
+    </section>
+  );
 };
 
 export default PopularMenu;
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+
 
 
 const Menu = () => {
@@ -12,11 +12,7 @@ const Menu = () => {
                 <title>Bistro Boss || Menu</title>
             </Helmet>
             <Cover img={menuImg} title={"Our Menu"}></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"Our Menu"}></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"Our Menu"}></Cover>
-            <PopularMenu></PopularMenu>
+            
         </div>
     );
 };
